90735f1c589fe76e151e3cf571a3e2a801b349b0
rockmove <rockinmove@gmail.com>
2024-11-28 16:51:19 +0900

    ✨ Feature : microcms-js-sdkの初期化のファイル作成

---
+++ src/app/lib/microcms/client.ts
@@ -0,0 +1,7 @@
+import { createClient } from "microcms-js-sdk";
+
+// microcms-js-sdkの初期化
+export const client = createClient({
+  serviceDomain: process.env.NEXT_PUBLICK_SERVICE_DOMAIN!,
+  apiKey: process.env.NEXT_PUBLICK_API_KEY!,
+});
