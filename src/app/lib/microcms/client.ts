a0e5fbb9ff13477095b74a7e23d1196889f0eac9
rockmove <rockinmove@gmail.com>
2024-11-28 17:30:12 +0900

    🔨 Fix : 変数名の修正

--- src/app/lib/microcms/client.ts
+++ src/app/lib/microcms/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "microcms-js-sdk";
 
 // microcms-js-sdkの初期化
-export const client = createClient({
+export const microcmsClient = createClient({
   serviceDomain: process.env.NEXT_PUBLICK_SERVICE_DOMAIN!,
   apiKey: process.env.NEXT_PUBLICK_API_KEY!,
 });
