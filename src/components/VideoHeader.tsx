fe8b049c380551e87e711ed54ecce021d4aee19e
rockmove <rockinmove@gmail.com>
2024-12-03 20:18:54 +0900

    ✨ Feature : 動画表示部分をコンポーネント化（後ほど修正する可能性大）

---
+++ src/components/VideoHeader.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const VideoHeader = () => {
+  return (
+    <div className='relative w-full h-screen overflow-hidden'>
+      <video
+        src='/20240217_am_dm3p_hd.mp4'
+        autoPlay
+        loop
+        muted
+        playsInline
+        className='absolute top-0 left-0 w-full h-full  object-cover object-center'
+      ></video>
+      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white'>
+        <h1>こんな感じのが流れるよ…</h1>
+      </div>
+    </div>
+  );
+};
+
+export default VideoHeader;
