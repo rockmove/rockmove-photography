60bd6bb9b2ccab760dfc2af6ca66f0a5debf9164
rockmove <rockinmove@gmail.com>
2024-12-03 21:23:14 +0900

    ✅ Test : framer-motionを用いたパララックス検証

--- src/components/VideoHeader.tsx
+++ src/components/VideoHeader.tsx
@@ -1,19 +1,54 @@
+"use client";
+
 import React from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const VideoHeader = () => {
+  const { scrollYProgress } = useScroll(); // スクロール進捗を取得
+  const scrollY = useTransform(scrollYProgress, [0, 1], [0, 1300]); // スクロール位置に基づく変換
+
+  // 背景動画のスクロールに応じた動き
+  const videoTransform = useTransform(
+    scrollY,
+    (value) => `translateY(${value * 1.0}px)`
+  );
+
+  // テキストのフェードインとスライドインのアニメーション
+  const textOpacity = useTransform(scrollY, [0, 150], [0, 1]);
+  const textTransform = useTransform(scrollY, [0, 300], [100, 0]);
+
   return (
     <div className='relative w-full h-screen overflow-hidden'>
-      <video
+      <motion.video
+        style={{
+          transform: videoTransform,
+        }}
         src='/20240217_am_dm3p_hd.mp4'
         autoPlay
         loop
         muted
         playsInline
         className='absolute top-0 left-0 w-full h-full  object-cover object-center'
-      ></video>
-      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white'>
-        <h1>こんな感じのが流れるよ…</h1>
-      </div>
+      ></motion.video>
+      <motion.div
+        style={{
+          opacity: textOpacity,
+          transform: textTransform,
+        }}
+        transition={{
+          opacity: { duration: 3, ease: "easeInOut" },
+          transform: { duration: 1, ease: [0.7, 0, 0.3, 1] },
+        }}
+        className='absolute bottom-8 w-full text-center'
+      >
+        <h1 className='text-white text-7xl'>
+          こんな感じのが流れるよ…
+          <br />
+          こんな感じのが流れるよ…
+          <br />
+          こんな感じのが流れるよ…
+        </h1>
+      </motion.div>
     </div>
   );
 };
