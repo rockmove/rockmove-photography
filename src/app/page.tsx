d661810675fe447f7a6268a4be23874862dc54e1
rockmove <rockinmove@gmail.com>
2024-12-06 13:36:07 +0900

    ✨ Feature : ページトップのパララックスをcssのstickyを使ったものに変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,22 +8,67 @@ import VideoHeader from "@/components/VideoHeader";
 export default function Home() {
   const [isInView, setIsInView] = useState(false); // 要素がビューポート内かどうか
 
+  useEffect(() => {
+    // IntersectionObserverの設定
+    const options = {
+      root: null, // ビューポートを基準に
+      rootMargin: "0px",
+      threshold: 0.3, // 30%が表示されたタイミングで反応
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const currentSection = entry.target;
+        const previousSection = currentSection.previousElementSibling;
+
+        // 次の要素がビューポートに30%〜50%入ったとき
+        if (entry.isIntersecting) {
+          // 前のセクションにブラーを適用
+          if (previousSection) {
+            previousSection.classList.add("blur-[2px]");
+          }
+        } else {
+          // セクションがビューポートから外れたとき
+          if (previousSection) {
+            previousSection.classList.remove("blur-[2px]");
+          }
+        }
+      });
+    }, options);
+
+    // 各セクションに対して監視を開始
+    const sections = document.querySelectorAll(".js-observed");
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+
+    // クリーンアップ
+    return () => {
+      sections.forEach((section) => {
+        observer.unobserve(section);
+      });
+    };
+  }, []);
+
   return (
     <div className='relative'>
       <Header isInView={isInView} />
       <VideoHeader setIsInView={setIsInView} />
-      <div className='h-screen w-screen flex justify-center items-center bg-slate-300'>
-        <TextBox />
-      </div>
-      <div className='h-screen w-screen flex justify-center items-center bg-white'>
-        <TextBox />
-      </div>
-      <div className='h-screen w-screen flex justify-center items-center bg-slate-300'>
-        <TextBox />
-      </div>
-      <div className='h-screen w-screen flex justify-center items-center bg-white'>
-        <TextBox />
-      </div>
+      <section className='js-observed sticky top-0 bg-slate-400 flex justify-center items-center min-h-screen text-white text-8xl'>
+        コンテンツ１
+      </section>
+      <section className='js-observed sticky top-0 bg-slate-500 flex justify-center items-center min-h-screen text-white text-8xl'>
+        コンテンツ２
+      </section>
+      <section className='js-observed sticky top-0 bg-slate-600 flex justify-center items-center min-h-screen text-white text-8xl'>
+        コンテンツ３
+      </section>
+      <section className='js-observed sticky top-0 bg-slate-700 flex justify-center items-center min-h-screen text-white text-8xl'>
+        コンテンツ４
+      </section>
+      <section className='js-observed sticky top-0 bg-slate-800 flex justify-center items-center min-h-screen text-white text-8xl'>
+        コンテンツ５
+      </section>
     </div>
   );
 }
