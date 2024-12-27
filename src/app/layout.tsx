24ff0e8addb291c57ec5eb4d84257a6915844b5f
rockmove <rockinmove@gmail.com>
2024-12-27 17:55:26 +0900

    ✨ Feature : faviconとogpの各種設定をmatadataに指定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,29 @@ import Footer from "@/components/Footer";
 export const metadata: Metadata = {
   title: "RockmovePhotography",
   description: "rockmoveことRyuheiHosomiが撮った風景写真を紹介します。",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: "RockmovePhotography",
+    description: "rockmoveことRyuheiHosomiが撮った風景写真を紹介します。",
+    url: "https://rockmovephotography.vercel.app/",
+    images: [
+      {
+        url: "/ogp.jpg",
+        width: 2048,
+        height: 1366,
+        alt: "元滝伏流水での光芒",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "RockmovePhotography",
+    description: "rockmoveことRyuheiHosomiが撮った風景写真を紹介します。",
+    images: ["/ogp.jpg"],
+  },
 };
 
 export default function RootLayout({
