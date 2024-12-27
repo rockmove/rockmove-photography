caced89e0ef70dee0397bb783d5a32af5a8e0327
rockmove <rockinmove@gmail.com>
2024-12-27 15:07:59 +0900

    🔨 Fix : 使用していない状態管理の削除

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,15 +9,12 @@ interface HeaderProps {
 }
 
 const Header = ({ isInView }: HeaderProps) => {
-  const [bgClass, setBgClass] = useState(""); // 背景色のクラス
   const [logoColor, setLogoColor] = useState(""); // ロゴの色
 
   useEffect(() => {
     if (!isInView) {
-      setBgClass("bg-white"); // 要素がビュー外になったとき
       setLogoColor("black"); // ロゴの色を変更
     } else {
-      setBgClass("bg-transparent"); // 元の背景色
       setLogoColor("white"); // 元のロゴ色
     }
   }, [isInView]); // トップページ判定やビューポート内外で変更
